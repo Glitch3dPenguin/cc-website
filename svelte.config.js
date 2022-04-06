96b7594fe6b82f64682b48b1bc760929402496da
Ender <33337309+e3ndr@users.noreply.github.com>
2022-04-06 17:00:26 -0500

    Ugh.

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,13 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    kit: {
+        adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false
+        })
+    }
 };
