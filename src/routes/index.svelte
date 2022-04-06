c32f8d7c355c9c6f3ab175cb60b1aeb19b307413
Ender <33337309+e3ndr@users.noreply.github.com>
2022-04-06 15:06:41 -0500

    Initial test commit.

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+	let count = 0;
+</script>
+
+<h1>Howdy!</h1>
+
+<br />
+
+<button on:click={() => count++}>Clicked: {count} times</button>
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	}
+};
+
+export default config;
